79267d1706b1b0374214d7b02506659e56d932cc
Juan Luis Talavera Peláez <jcooldevelopment@gmail.com>
2024-05-06 14:22:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Noticias al día',
+  webDir: 'www'
+};
+
+export default config;
