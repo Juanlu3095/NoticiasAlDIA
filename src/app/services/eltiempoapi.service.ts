b3e2f3162e6fc25feeac95caba26527b8ae35247
Juanlu3095 <juanlu_ambientales@hotmail.com>
2024-06-26 18:42:10 +0200

    26/6/2024 ver 1.0

--- src/app/services/eltiempoapi.service.ts
+++ src/app/services/eltiempoapi.service.ts
@@ -4,28 +4,12 @@ import { environment } from 'src/environments/environment';
 import { switchMap } from 'rxjs';
 import { text } from 'ionicons/icons';
 
-const apikeytiempo = environment.apikeyeltiempo;
-
 @Injectable({
   providedIn: 'root'
 })
 export class EltiempoapiService {
 
   constructor(private http: HttpClient) { }
-
-  // Obtiene mapa de isobaras en formato blob
-  getisobaras(){
-    return this.http.get<any>('https://opendata.aemet.es/opendata/api/mapasygraficos/analisis', {
-      params: {
-        'api_key': apikeytiempo
-      }
-    }).pipe(
-      switchMap( (response) => { // Switch map transforma valores de un observable en otro observable y los fusiona en uno solo, por tanto devuelve la última llamada HTTP
-        const url = response['datos'];
-        return this.http.get(url, { responseType: 'blob' }); // Los datos que se devuelven son en formato binario y para obtener la img hay que usar blob para guardarlo en memoria
-      })
-    )
-  }
 
   // Obtiene mapa de isobaras en formato blob con la apikey de la base de datos
   getisobarasconapi(apikey:string){
@@ -39,47 +23,6 @@ export class EltiempoapiService {
         return this.http.get(url, { responseType: 'blob' }); // Los datos que se devuelven son en formato binario y para obtener la img hay que usar blob para guardarlo en memoria
       })
     )
-  }
-
-  // Devuelve el pdf, ver este enlace: https://stackoverflow.com/questions/50332447/how-to-get-pdf-through-api-in-angular-service
-  getBalanceHidrico(año:number, decena:number){
-    return this.http.get(`https://opendata.aemet.es/opendata/api/productos/climatologicos/balancehidrico/${año}/${decena}`, {
-      params: {
-        'api_key': apikeytiempo
-      },
-      responseType: text
-    } as Record<any, unknown>)
-  }
-
-  // Obtenemos todos los municipios
-  getMunicipios(){
-    return this.http.get('https://opendata.aemet.es/opendata/api/maestro/municipios', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    })
-  }
-
-  getInfoMunicipio(){
-    return this.http.get('https://opendata.aemet.es/opendata/api/maestro/municipio/44004', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    })
-  }
-
-  // Predicción nacional para hoy en texto
-  getPrediccionNacionalHoy(){
-    return this.http.get<any>('https://opendata.aemet.es/opendata/api/prediccion/nacional/hoy', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    }).pipe(
-      switchMap(response => {
-        const datosUrl = response.datos;
-        return this.http.get(datosUrl, {responseType: 'text'}); // En este caso se obtiene un texto plano, por ello hay que indicar el tipo de respuesta para que no haya error.
-      })
-    );
   }
 
   // Predicción nacional para hoy en texto con la apikey de la base de datos
@@ -96,50 +39,6 @@ export class EltiempoapiService {
     );
   }
 
-  // Predicción nacional para mañana en texto
-  getPrediccionNacionalManana(){
-    return this.http.get<any>('https://opendata.aemet.es/opendata/api/prediccion/nacional/manana', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    }).pipe(
-      switchMap(response => {
-        const datosUrl = response.datos;
-        return this.http.get(datosUrl, {responseType: 'text'}); // En este caso se obtiene un texto plano, por ello hay que indicar el tipo de respuesta para que no haya error.
-      })
-    );
-  }
-
-  getinfocosta(){
-    return this.http.get('https://opendata.aemet.es/opendata/api/prediccion/maritima/costera/costa/42', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    })
-  }
-
-  getClimatologia(){
-    return this.http.get('https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/2024-05-18/fechafin/2024-05-19/todasestaciones', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    })
-  }
-
-  // Devuelve Tº, humedad, precipitaciones probabilidad, tiempo en texto. El código de municipio viene en getMunicipios
-  getPrediccionMunicipio(){
-    return this.http.get<any>('https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/29067', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    }).pipe(
-      switchMap( (response) => { // Switch map transforma valores de un observable en otro observable y los fusiona en uno solo, por tanto devuelve la última llamada HTTP
-        const url = response.datos;
-        return this.http.get(url); // Los datos que se devuelven son en formato binario y para obtener la img hay que usar blob para guardarlo en memoria
-      })
-    )
-  }
-
   // Devuelve Tº, humedad, precipitaciones probabilidad, tiempo en texto. El código de municipio viene en getMunicipios utilzando los datos de la base de datos
   getPrediccionMunicipioconapi(apikey:string, codmunicipio:number){
     return this.http.get<any>(`https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/${codmunicipio}`, {
@@ -152,15 +51,6 @@ export class EltiempoapiService {
         return this.http.get(url); // Los datos que se devuelven son en formato binario y para obtener la img hay que usar blob para guardarlo en memoria
       })
     )
-  }
-
-  // Devuelve el tiempo de hoy por provincia en texto
-  getPrediccionProvinciaHoy(){
-    return this.http.get('https://opendata.aemet.es/opendata/api/prediccion/provincia/hoy/29', {
-      params: {
-        'api_key': apikeytiempo
-      },
-    })
   }
 
 }
--- src/app/services/newsapi.service.ts
+++ src/app/services/newsapi.service.ts
@@ -3,8 +3,6 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { map, catchError, Observable, throwError } from 'rxjs';
 
-const apikey = environment.apikeynews;
-
 @Injectable({
   providedIn: 'root'
 })
@@ -14,18 +12,6 @@ export class NewsApiService {
   endpointIndividualNew = 'https://api.worldnewsapi.com/extract-news?';
 
   constructor(private http: HttpClient) { }
-
-  // Obtenemos la noticia concreta por medio de la url (La API no permite usar la id para pasarla por parámetro)
-  getIndividualNew(url:string): Observable<any> {
-
-    return this.http.get(this.endpointIndividualNew, {
-      params: {
-        'api-key': apikey,
-        'url': url,
-        'analyze': false // Extrae entidades si es true
-      }
-    })
-  }
 
   // Obtenemos la noticia concreta por medio de la url (La API no permite usar la id para pasarla por parámetro). Usamos la Apikey desde la base de datos.
   getIndividualNewConApi(url:string, apikeynoticias:string): Observable<any> {
@@ -37,42 +23,6 @@ export class NewsApiService {
         'analyze': false // Extrae entidades si es true
       }
     })
-  }
-
-  // Obtenemos noticias por el lugar donde ocurre. Usado para pruebas en desarrollo.
-  getNewsByPlace(entity: string) {
-    return this.http.get(this.endpoint, {
-      params: {
-        'api-key': apikey,
-        //'text': 'malaga', // Palabra clave
-        //'earliest-publish-date': '2024-05-01', // Fecha más antigua de publicación
-        'sort': 'publish-time', // Ordenar por fecha de publicación
-        'sort-direction': 'DESC', // Ordenar por orden descendiente: De más moderno a más antiguo
-        'source-countries': 'es', // País del diario que redacta la noticia
-        'number': '20', // Número máximo de noticias
-        'entities': `LOC:${entity}` // Entidad, puede ser lugar, persona u organización
-      }
-    }).pipe(
-      map((res:any) => { /* Mapear nos permite acceder directamente al data dentro del json para no tener que poner siempre data.loquesea */
-        return res.news;
-      }), // Podemos poner un tipo detrás del otro con una coma
-      catchError( (error: HttpErrorResponse) => {
-        let errorMessage = '';
-  
-        if(error.error instanceof ErrorEvent) {
-          errorMessage = `Error: ${error.error.message}`; // ERROR DEL CLIENTE
-        } else { // ERRORES DEL SERVIDOR
-          if( error.status === 402) {
-            errorMessage = `Su petición no se puede procesar porque ha excedido el límite de uso diario para su cuenta.`; 
-          } else {
-            errorMessage = `Error code: ${error.status}, message: ${error.message}`; 
-          }
-           
-        }
-  
-        return throwError( () => errorMessage);
-      })
-    )
   } 
 
   // Obtenemos noticias por el lugar donde ocurre utilizando la apikey de la base de datos
@@ -87,25 +37,6 @@ export class NewsApiService {
         'source-countries': 'es', // País del diario que redacta la noticia
         'number': '20', // Número máximo de noticias
         'entities': `LOC:${entity}` // Entidad, puede ser lugar, persona u organización
-      }
-    }).pipe(
-      map((res:any) => { /* Mapear nos permite acceder directamente al data dentro del json para no tener que poner siempre data.loquesea */
-        return res.news;
-      })
-    )
-  }
-
-  // Obtenemos noticias por palabra clave. Usado para pruebas en desarrollo.
-  getNewsByKeyword(keyword: string) {
-    return this.http.get(this.endpoint, {
-      params: {
-        'api-key': apikey,
-        'text': keyword, // Palabra clave
-        'earliest-publish-date': '2024-05-01', // Fecha más antigua de publicación
-        'sort': 'publish-time', // Ordenar por fecha de publicación
-        'sort-direction': 'DESC', // Ordenar por orden descendiente: De más moderno a más antiguo
-        'source-countries': 'es', // País del diario que redacta la noticia
-        'number': '20', // Número máximo de noticias
       }
     }).pipe(
       map((res:any) => { /* Mapear nos permite acceder directamente al data dentro del json para no tener que poner siempre data.loquesea */
