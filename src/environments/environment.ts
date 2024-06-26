8298299dc0838d80d23d0005a3066b828b6f9b09
Juanlu <85942298+Juanlu3095@users.noreply.github.com>
2024-06-26 18:44:37 +0200

    Update environment.ts
    Preparación proyecto firebase.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,13 @@ export const environment = {
 
   // Configuración de FIREBASE
   firebaseConfig: {
-    apiKey: "AIzaSyCg3G4SvBuulkLDtN_praP-JXxZMpyOYsc",
-    authDomain: "noticiasaldia-app.firebaseapp.com",
-    projectId: "noticiasaldia-app",
-    storageBucket: "noticiasaldia-app.appspot.com",
-    messagingSenderId: "665761812694",
-    appId: "1:665761812694:web:a12d9eb74c72bd15edb977",
-    measurementId: "G-864NCZ14JM"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
