c7feadf84dcd08af4e408c7428042c9f9fa39454
Juanlu <85942298+Juanlu3095@users.noreply.github.com>
2024-06-27 15:07:53 +0200

    Update eltiempoapi.service.ts
    Correción service eltiempo

--- src/app/services/eltiempoapi.service.ts
+++ src/app/services/eltiempoapi.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { switchMap } from 'rxjs';
-import { text } from 'ionicons/icons';
 
 @Injectable({
   providedIn: 'root'
